8381ca045c6bc4ae45f5ec985baef00fba4a38cd
idcrpz <ingrid.corpuz11@gmail.com>
2025-03-13 22:31:40 +0800

    Added dynamic blog page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +18,18 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Serve the /blog route dynamically from the posts.json file
+app.get('/blog', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send("Error reading blog data.");
+        }
+
+        const posts = JSON.parse(data);  // Parse the JSON data
+        res.render('blog', { posts });  // Render blog.html with posts
+    });
 });
 
 // Start the server
